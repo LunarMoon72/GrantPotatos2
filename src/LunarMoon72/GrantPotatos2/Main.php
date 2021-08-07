1ae793a07220ff871eb10685ff58c099b6cd5e80
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-07 11:11:58 -0400

    Create Main.php

---
+++ src/LunarMoon72/GrantPotatos2/Main.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LunarMoon72\GrantPotatoes;
+
+use LunarMoon72\GrantPotatoes\Item;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase implements Listener{
+   public function onEnabled(){
+   	$this->getServer()->getPluginManager()->registerEvents($this,$this);
+   	$this->getLogger()->info("Plugin is Enabled");
+
+   }
+   
+   public function onDisabled(){
+   	$this->getLogger()->info("Plugin is Disabled");
+   }
+public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+   switch($cmd->getName()){
+     case "test":
+       if($sender->hasPermission("potato.command")){
+         if(!$sender instanceof Player){
+           $sender->sendMessage("This Command Only Works In-Game!");
+         }else{
+           if(!isset($args[0]) or (is_int($args[0]) and $args[0] > 0)){ 
+            $args[0] = 1; 
+           }
+           $sender->getInventory()->addItem(Item::get(393,0,$args[0]));
+           $sender->sendMessage("You have just recieved" .count($args[0]). " baked potatoes!");
+         }
+       }else{
+         $sender->sendMessage("You don't have permission to use these potatoes");
+       }
+     break;
+  }
+  return true;
+}
+
+}
